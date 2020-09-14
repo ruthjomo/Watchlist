fdc1396f9f3913fdbc8c1881225e773230594616
ruthjomo <ruthjomo19@gmail.com>
2020-09-14 10:13:56 +0300

    deleted files and added codes

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
+def index():
 
     '''
-    View movie page function that returns the movie details page and its data
+    View root page function that returns the index page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
