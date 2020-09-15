de52a0e926f3a16c3ab9811b363feffb8e84fd6a
ruthjomo <ruthjomo19@gmail.com>
2020-09-15 07:22:11 +0300

    added more codes in the file

--- app/views.py
+++ app/views.py
@@ -6,3 +6,18 @@ def index():
 
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title)
+
+    from .request import get_movies
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
